acc911e1901acf5c311dc4d6b76e6ff88df0db96
DeCode2018 <decode2018@gmail.com>
2018-08-17 18:09:44 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown (time)
+  number = time
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number-=1
+  end
+    "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(time)
+
+  number = time
+  while number > 0
+    sleep(1)
+    puts "#{number} SECOND(S)!"
+    number-=1
+  end
+    "HAPPY NEW YEAR!"
 end
